4e0f1452a4bec7bacf4d6a4c2b999fd6e6d016ec
Elvira <akzholtursunov01@gmail.com>
2022-01-17 21:59:04 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+            int number = 0;
+        for (int i = 0; i <=100 ; i++) {
+            if ( i % 3 == 0) {
+                System.out.println(i);
+                number++;
+
+            }
+            
+        }
+
+            }
+        }
+
+
